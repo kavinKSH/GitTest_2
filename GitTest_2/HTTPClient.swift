5ba8f7a83dc6e9a9ce2a16b29a9eabe776795011
kavinKSH <kavin14795@gmail.com>
2024-05-25 15:53:30 +0530

    [Future] Networking Call

---
+++ GitTest_2/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPClient.swift
+//  GitTest_2
+//
+//  Created by Kavin on 25/05/24.
+//
+
+import Foundation
+
+class HTTPClient {
+    private func networking () {
+        print("URLS")
+    }
+}
