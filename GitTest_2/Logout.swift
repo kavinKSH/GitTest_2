9621d97c20eb9e014649bfc001fae10c49c667c5
kavinKSH <kavin14795@gmail.com>
2024-05-27 12:38:37 +0530

    [Future] create and validate logout screen

---
+++ GitTest_2/Logout.swift
@@ -0,0 +1,19 @@
+//
+//  Logout.swift
+//  GitTest_2
+//
+//  Created by Kavin on 27/05/24.
+//
+
+import Foundation
+
+class Logout {
+    
+    private init() {}
+    
+    static let shared = Logout()
+    
+    func logoutActions() {
+        print("are you really want close the app!")
+    }
+}
