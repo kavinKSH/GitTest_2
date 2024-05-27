d6c77cec744e39881d820c5e1b2486d19828660d
kavinKSH <kavin14795@gmail.com>
2024-05-27 12:14:08 +0530

    [Future] Create Log in class

---
+++ GitTest_2/LogIn.swift
@@ -0,0 +1,16 @@
+//
+//  LogIn.swift
+//  GitTest_2
+//
+//  Created by Kavin on 27/05/24.
+//
+
+import Foundation
+
+class LogIn {
+    private init () {}
+    
+    func getUserInfo() {
+        print("Log In users")
+    }
+}
