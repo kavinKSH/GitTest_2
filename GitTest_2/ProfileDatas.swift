4f714a8ee8e256d71202e455941f69ac854de08b
kavinKSH <kavin14795@gmail.com>
2024-05-28 12:34:37 +0530

    [Future] Create Profile details

---
+++ GitTest_2/ProfileDatas.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileDatas.swift
+//  GitTest_2
+//
+//  Created by Kavin on 28/05/24.
+//
+
+import Foundation
+
+struct ProfileDatas {
+    var name: String = ""
+    var dateOfBirth = ""
+    var idNumbers: Int = 0
+}
